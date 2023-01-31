6ca07755cf48f11abe1b7b143e185f1a0eb7f2e9
wappon_28_dev <wappon_28_dev@ruru.be>
2023-02-01 02:00:59 +0900

    :sparkles: フロントエンドのテンプレート適用

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Card from './card.svelte';
+</script>
+
+<div class="wide_title">
+	<p>A svelte project template with<br />sveltekit & Svelte Material UI</p>
+</div>
+
+<main>
+	<Card />
+</main>
+
+<style lang="scss">
+	main {
+		padding: 0 100px;
+		max-width: 1400px;
+		margin: 0 auto;
+
+		@media screen and (max-width: 860px) {
+			padding: 0 20px;
+		}
+	}
+</style>
--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,15 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+export default {
+	preprocess: sveltePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: true,
+			strict: true
+		})
 	}
 };
-
-export default config;
