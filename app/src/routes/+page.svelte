44e5e0c9deff26d27e89aa68eb4fc4fa2bdc5d2c
samurai-7xy <108717711+samurai-7xy@users.noreply.github.com>
2023-04-19 18:11:59 +0900

    スタートとアカウントのボタンの追加

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -40,6 +40,10 @@
   <Button on:click={onButtonClicked} variant="raised">
     <Label>テストボタン</Label>
   </Button>
+  
+  <div><p><a href="/quiz" class="btn">スタート</a></p></div>
+  <div><p><a href="/user" class="btn">アカウント</a></p></div>
+    
 </main>
 
 <style lang="scss">
@@ -50,4 +54,19 @@
       font-size: 30px;
     }
   }
+  
+  div .btn{
+    border-radius: 5px;
+    background-color: var(--m3-primary);
+    padding: 5px;
+    margin: 10px;
+    text-decoration: none;
+    color: white;
+    box-shadow: 2px 2px 4px -1px gray;
+  }
+  
+  div p .btn{
+    text-align: center;
+  }
+  
 </style>
